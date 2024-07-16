8e7746607e90ceb367b1d4ba52666d1d1e951866
usamanadeemdeveloper <usamanadeemparacha@gmail.com>
2024-07-17 02:47:54 +0500

    Streamer Section Has Implemented

---
+++ frontend/src/app/interface/StreamerSection.ts
@@ -0,0 +1,11 @@
+export interface StreamerSection {
+  id: number | undefined | null;
+  carouselImages: string[] | undefined | null;
+  title: string | undefined | null;
+  subtitle: string | undefined | null;
+  description: string | undefined | null;
+  headings: string[] | undefined | null;
+  texts: string[] | undefined | null;
+  buttonText: string | undefined | null;
+  buttonLink: string | undefined | null;
+}
